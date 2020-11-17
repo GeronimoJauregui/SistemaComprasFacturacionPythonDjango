22198ea4b4d7f0449ddee9628921c7e1ac3ad6a8
GeronimoJauregui <geronimojaureguimartinez@gmail.com>
2020-11-17 02:08:44 -0600

    Migración Categoria

--- inv/models.py
+++ inv/models.py
@@ -2,4 +2,17 @@ from django.db import models
 from bases.models import ClaseModelo
 
 class Categoria(ClaseModelo):
-    
+    descripcion = models.CharField(
+        max_length=100,
+        help_text="Descripción de la categoria",
+        unique=True
+    )
+    def __str__(self):
+        return '{}'.format(self.descripcion) #Convertir el la descripcion a texto entendible
+
+    def save(self):
+        self.descripcion = self.descripcion.upper() #Guardar la descripcion en mayusculas
+        super(Categoria, self)
+
+    class Meta:
+        verbose_name_plural= "Categorias"
