e48d9f2fa4ee9b843ef9a2f53ede428611fced0c
GeronimoJauregui <geronimojaureguimartinez@gmail.com>
2021-01-08 00:10:04 -0600

    Creacion del EndPoint desde la parte de BackEnd

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
-
+from .views import ProductoList, ProductoDetalle
 urlpatterns = [
-    
+    path('v1/productos/',ProductoList.as_view(),name='producto_list'),
+    path('v1/productos/<str:codigo>',ProductoDetalle.as_view(),name='producto_detalle')
 ]
+
+# 127.0.0.1:8000/api/v1/productos/
